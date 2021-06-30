9fb603114ca2683b740e46e90ce9133aa08c5be4
Taido7478 <86735057+Taido7478@users.noreply.github.com>
2021-06-30 23:14:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    introTitle: 'Em à', Yêu em nhiềuuu.Sinh nhật vui vẻ :)) 
+Quanh ta có rất nhiều người vậy mà chúng ta lại tìm được nhau
+    btnIntro: 'hí :))',
+    title: 'Em yêu, yêu anh chớ hửmmm',
+    desc: 'Em yêu, yêu anh chớ hửmmm',
+    btnYes: 'Vẫn cứ là yêu anh đẹp trai mà <33',
+    btnNo: 'Khong , đồ khùngg:3',
+    question:'Quanh ta có rất nhiều người tại sao mình lại thuộc về nhau <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu mà không yêu thì yêu :))',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Tối nay anh chở đi chơi sinh nhật nhée.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
